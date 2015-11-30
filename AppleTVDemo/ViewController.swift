7dc5f9485878aff82c9e3eabc27238b9aa0066aa
Oleksii Ozun <oozun@softserveinc.com>
2015-12-01 00:53:24 +0200

    Initial Commit

---
+++ AppleTVDemo/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  AppleTVDemo
+//
+//  Created by Oleksii Ozun on 01.12.15.
+//  Copyright © 2015 DataArt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
